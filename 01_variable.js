cca8dd27dbbdf010e23e989f830e1010bf041ebf
suhasjavir <suhas.javir9850@gmail.com>
2022-12-08 22:19:13 +0530

    starting javascript
    starting javascript

---
+++ 01_variable.js
@@ -0,0 +1,7 @@
+var myName; //variable Declaration
+myName = "Suhas Javir"; //vaiable initilization 
+console.log(myName);
+var myFrdName = "Sachin Tendulkar"; //variable declaraton and initialization at the same time
+console.log(myFrdName); 
+myFrdName = "MS Dhoni"; //variable Update
+console.log(myFrdName);
---
+++ 01_variable_assign.js
@@ -0,0 +1,15 @@
+var myFullName;
+myFullName ="Suhas Javir";
+console.log(myFullName);
+myFullName ="Suhas Kundlik Javir";
+console.log(myFullName);
+var myLove = "I love only JS";
+console.log(myLove);
+var firstName = "Suhas";
+var lastName = "Javir";
+var age = "32";
+var address = "Chavhan Nagar, Dhankawadi, Pune.";
+var pin_code = "411 043";
+var gender = "male";
+var is_married = "yes";
+console.log(firstName, lastName, age, address, pin_code, gender, is_married);
