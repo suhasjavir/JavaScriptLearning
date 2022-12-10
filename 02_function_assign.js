bf42c35aa4bf60f0e746161842763a786c7a1e1e
suhasjavir <suhas.javir9850@gmail.com>
2022-12-11 01:28:16 +0530

    Functions Assign
    Functions Assign

---
+++ 02_function_assign.js
@@ -0,0 +1,33 @@
+
+function lineOne(){
+    console.log("Do it now.");
+}
+function lineTwo(){
+    console.log("Sometimes 'later' becomes 'never'.");
+}
+lineOne();
+lineTwo();
+console.log("=============================================");
+
+function personalDetails(firstName, lastName, collageName){
+    console.log("first Name:", firstName);
+    console.log("Last Name:", lastName);
+    console.log("Collage Name:", collageName);
+}
+personalDetails("Suhas", "Javir", "Tilak Maharastra University");
+console.log("=============================================");
+function swapValuesDude(val1, val2){
+    console.log("Befor Swap:", val1, val2);
+    var temp = val1;
+    val1 =val2;
+    val2 = temp;
+    console.log("After Swap:", val1, val2);
+}
+swapValuesDude("virat", "Anushka");
+swapValuesDude(1000, 2000);
+console.log("=============================================");
+function addThreeValues(no1, no2, no3){
+    console.log("Addition Of Given Three Values:", no1 + no2 + no3);
+}
+addThreeValues(10.23, 600,40);
+addThreeValues("Hello", "Good", "Morning");
---
+++ 02_function_pract.js
@@ -0,0 +1,37 @@
+//*******************************************function with no argument and no return type.
+function display(){       //creating a function.
+    console.log("My name: Suhas.");     //to print this value
+    console.log("My sirname is: Javir.");    //to print this value
+}
+display();  //call or invocation the function
+
+function show(){
+    console.log("My Collage Name: Tilak Maharashtra Univercity.");
+}
+show();
+//*******************************************function with argument and no return type.
+function showDetails(myName){
+console.log("Details", myName);
+}
+showDetails("Suhas Javir");
+showDetails("Popat Madane");
+showDetails("Rupali Arjun");
+//*********************************************************
+// var value1 = 100;
+// var value2 = 200;
+// console.log("Befor Swap", value1, value2);
+// var temp = value1;
+// value1 = value2;
+// value2 = temp;
+// console.log("Befor Swap", value1, value2);
+function swapValues(value1, value2){
+console.log("Befor Swap", value1, value2);
+var temp = value1;
+value1 = value2;
+value2 = temp;
+console.log("After Swap", value1, value2);
+}
+swapValues(100, 200);
+swapValues(2, 4);
+swapValues("kishor", "hitesh");
+swapValues("you", "me");
